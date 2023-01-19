8c2cd7a13490a14ed41740c5f8ebe8d4f2cca497
Xinqian Dai <116857377+xDai0706@users.noreply.github.com>
2023-01-18 22:35:44 -0500

    Add files via upload

---
+++ 1.18.py
@@ -0,0 +1,34 @@
+#linear regression
+
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+print(data.head())
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1)) # Features
+y = np.array(data[predict]) # Labels
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test) # acc stands for accuracy
+
+print(acc)
+
+print('Coefficient: \n', linear.coef_) # These are each slope value
+print('Intercept: \n', linear.intercept_) # This is the intercept
+
+predictions = linear.predict(x_test) # Gets a list of all predictions
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
