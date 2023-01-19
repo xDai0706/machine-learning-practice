37d40e87ad60abd1bac81f2bc75f6d8a3f3e2034
Xinqian Dai <116857377+xDai0706@users.noreply.github.com>
2023-01-19 15:59:27 -0500

    Add files via upload

---
+++ 1.19 neural network.py
@@ -0,0 +1,39 @@
+#neural networ（prediction）
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+model = keras.Sequential([
+	keras.layers.Flatten(input_shape=(28,28)),
+	keras.layers.Dense(128, activation="relu"),
+	keras.layers.Dense(10, activation="softmax")
+	])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print(test_acc)
+
+predictions = model.predict(test_images)
+
+plt.figure(figsize=(5,5))
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[test_labels[i]])
+    plt.title(class_names[np.argmax(predictions[i])])
+    plt.show()
